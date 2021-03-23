501836dbb86dd6eadaf54fa9ecb614ef684d492e
Lo-GR <diamondintheroth@gmail.com>
2021-03-23 15:17:13 -0700

    add author create routes and view
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -19,5 +19,18 @@ namespace Library.Controllers
       List<Author> model = _db.Authors.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Author author)
+    {
+      _db.Authors.Add(author);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
