1a43bf0f944eaed4ab436d2a6fde0a8b534a6b1b
Lo-GR <diamondintheroth@gmail.com>
2021-03-23 14:44:32 -0700

    add author controller and index
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -1,5 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Library.Models;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace Library.Controllers
+{
+  public class AuthorsController : Controller
+  {
+    private readonly LibraryContext _db;
+    public AuthorsController(LibraryContext db)
+    {
+      _db = db; 
+    }
+    public ActionResult Index()
+    {
+      List<Author> model = _db.Authors.ToList();
+      return View(model);
+    }
+  }
+}
